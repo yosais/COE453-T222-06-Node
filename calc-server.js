8e983abd955415121f6751ba43ca5cb682e28fad
yosais <yosais@kfupm.edu.sa>
2023-04-30 09:02:53 +0300

    1 file added

---
+++ calc-server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/add', (req, res) => {
+  let num1 = Number(req.query.num1);
+  let num2 = Number(req.query.num2);
+  res.send(`Sum = ${num1 + num2}`);
+});
+
+app.listen(3000, function () {
+  console.log('Server running!');
+});

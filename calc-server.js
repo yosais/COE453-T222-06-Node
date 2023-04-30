ef8d07e728f1e41e086a7d92e78097747a0a7c88
yosais <yosais@kfupm.edu.sa>
2023-04-30 09:24:23 +0300

    1 file modified

--- calc-server.js
+++ calc-server.js
@@ -6,9 +6,17 @@ app.get('/', function (req, res) {
   res.send('Calculator Example');
 });
 
+// Passing parameters in a query string
 app.get('/add', (req, res) => {
   let num1 = Number(req.query.num1);
   let num2 = Number(req.query.num2);
+  res.send(`Sum = ${num1 + num2}`);
+});
+
+//Passing parameters in a JSON file
+app.get('/sub', (req, res) => {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
   res.send(`Sum = ${num1 + num2}`);
 });
 

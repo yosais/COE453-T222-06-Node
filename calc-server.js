10dd6c02b3793fc346357409b7f8fbf19d860c00
yosais <yosais@kfupm.edu.sa>
2023-04-30 09:08:03 +0300

    1 file modified

--- calc-server.js
+++ calc-server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', function (req, res) {
+  res.send('Calculator Example');
+});
+
 app.get('/add', (req, res) => {
   let num1 = Number(req.query.num1);
   let num2 = Number(req.query.num2);

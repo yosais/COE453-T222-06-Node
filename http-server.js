bb895873fc59a6aa97c059bc570167d0aacf9e2d
yosais <yosais@kfupm.edu.sa>
2023-04-30 08:41:17 +0300

    1 file modified

--- http-server.js
+++ http-server.js
@@ -1,5 +1,12 @@
 const http = require('http');
+
 const server = http.createServer();
+
+server.on('request', function (req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
 
 server.on('listening', function () {
   console.log('Server running!');

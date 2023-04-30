e6a7e957c70de19f9af30b69061c35020b92faed
yosais <yosais@kfupm.edu.sa>
2023-04-30 09:43:02 +0300

    4 files modified

--- calc-client.js
+++ calc-client.js
@@ -1,8 +1,19 @@
 import fetch from 'node-fetch';
 
 // Get
+const customHeaders = {
+  mode: 'cors',
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
+};
+
 fetch(
-  'https://nodezjchev-ysxc--3000--d830b1a2.local-credentialless.webcontainer.io/add?num1=2&num2=3'
+  'https://nodezjchev-ysxc--3000--d830b1a2.local-credentialless.webcontainer.io/add?num1=2&num2=3',
+  {
+    method: 'GET',
+    headers: customHeaders,
+  }
 )
   .then((res) => res.json())
   .then((json_obj) => console.log(json_obj.data.first_name))
--- calc-server.js
+++ calc-server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', function (req, res) {
   res.send('Calculator Example');

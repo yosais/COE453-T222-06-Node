4fe071d82d416ea64a3512f13f4b8fa49f6aba81
yosais <yosais@kfupm.edu.sa>
2023-04-30 10:27:15 +0300

    1 file modified

--- mpa-server.js
+++ mpa-server.js
@@ -5,8 +5,6 @@ const port = 7000;
 // Use the static middleware
 app.use(express.static('public'));
 
-app.get;
-
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}`);
 });
